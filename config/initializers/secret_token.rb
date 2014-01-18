3f5158bfe7907d933d51df5753e50119cf9ad0ad
kg <kennytgao@gmail.com>
2014-01-17 17:40:20 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HsDeckLogger::Application.config.secret_key_base = 'c942e19d487690529392af78971538c4cbd74542ea8502cc323b1a14f60c4dd356806e506db9be90d4ed357ce4f144b6d8b68fb17108d36ce94d263eaeea528c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HsDeckLogger::Application.config.session_store :cookie_store, key: '_hs-deck-logger_session'

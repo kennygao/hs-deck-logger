33d3e3dd3da7c70f06fcac82e9902cea5dcb7fef
kg <kennytgao@gmail.com>
2014-01-29 17:21:56 -0800

    implemented basic model validations

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,3 +1,5 @@
 class Deck < ActiveRecord::Base
   has_many :records, dependent: :destroy
+
+  validates :name, length: { maximum: 30 }, presence: true
 end
--- app/models/record.rb
+++ app/models/record.rb
@@ -1,3 +1,11 @@
 class Record < ActiveRecord::Base
   belongs_to :deck
+
+  HEROES = ['Warrior', 'Shaman', 'Rogue', 'Paladin', 'Hunter', 'Druid', 'Warlock', 'Mage', 'Priest']
+
+  validates :opponent_name, length: { maximum: 30 }, presence: true
+  validates :opponent_hero, inclusion: 0...HEROES.length, presence: true
+  # validates :first
+  # validates :win
+  # validates :notes
 end
